ca69a79441576812254a5ae0405ad5b864164310
kitano-kazuki <kitano-kazuki@users.noreply.github.com>
2024-10-10 03:16:15 +0000

    Apply automatic changes

--- src/components/bot/NewBotComponent.tsx
+++ src/components/bot/NewBotComponent.tsx
@@ -1,15 +1,14 @@
-import * as React from 'react';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
+import * as React from 'react';
 
 const bull = (
   <Box
-    component="span"
+    component='span'
     sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
   >
     •
@@ -18,20 +17,21 @@ const bull = (
 
 export default function NewBotComponent() {
   return (
-    <Card sx={{maxWidth: 'lg', minWidth : '50%'}}>
+    <Card sx={{ maxWidth: 'lg', minWidth: '50%' }}>
       <CardContent>
-        <Typography variant="h2" component="div" sx={{width: '60%'}}>
-           Friend Profile
+        <Typography variant='h2' component='div' sx={{ width: '60%' }}>
+          Friend Profile
         </Typography>
         <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
-        <Typography variant="body2">
+        <Typography variant='body2'>
           well meaning and kindly.
           <br />
           {'"a benevolent smile"'}
         </Typography>
       </CardContent>
-      <CardActions>BasicCard
-        <Button size="small">Learn More</Button>
+      <CardActions>
+        BasicCard
+        <Button size='small'>Learn More</Button>
       </CardActions>
     </Card>
   );
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode; }) {
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
-        {children}
+      {children}
     </div>
   );
 }
