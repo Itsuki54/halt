97f9443e276acff31f04969a878d2ddd17e58dfd
kitano kazuki <kitano-kazuki954@g.ecc.u-tokyo.ac.jp>
2024-10-10 15:33:23 +0900

    Merge develop into ui

--- src/hooks/recording.ts
+++ src/hooks/recording.ts
@@ -1,4 +1,5 @@
 import {
+  useEffect,
   useRef,
   useState,
 } from 'react';
@@ -7,15 +8,18 @@ type Hooks = {
   startRecording: () => void;
   stopRecording: () => void;
   isAudio: boolean;
+  transcript: string | null;
+  isLoading: boolean;
 };
 
 export const useHooks = (): Hooks => {
   const mediaRecorder = useRef<MediaRecorder | null>(null);
   const [audioFile, setAudioFile] = useState<File | null>(null);
   const [isAudio, setIsAudio] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [transcript, setTranscript] = useState<string | null>(null);
 
   const handleDataAvailable = (event: BlobEvent) => {
-    // 音声ファイル生成
     const file = new File([event.data], 'audio.mp3', {
       type: event.data.type,
       lastModified: Date.now(),
@@ -24,7 +28,6 @@ export const useHooks = (): Hooks => {
   };
 
   const startRecording = async () => {
-    // 録音開始
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
     mediaRecorder.current = new MediaRecorder(stream);
     mediaRecorder.current.start();
@@ -36,14 +39,43 @@ export const useHooks = (): Hooks => {
   };
 
   const stopRecording = () => {
-    // 録音停止
     mediaRecorder.current?.stop();
     setIsAudio(false);
   };
+
+  useEffect(() => {
+    const uploadAudio = async () => {
+      if (!audioFile) return;
+      const endPoint = 'https://api.openai.com/v1/audio/transcriptions';
+
+      const formData = new FormData();
+      formData.append('file', audioFile, 'audio.mp3');
+      formData.append('model', 'whisper-1');
+      formData.append('language', 'ja');
+      setIsLoading(true);
+      const response = await fetch(endPoint, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: formData,
+      });
+      console.log(response);
+      const responseData = await response.json();
+      if (responseData.text) {
+        setTranscript(responseData.text);
+      }
+      setAudioFile(null);
+      setIsLoading(false);
+    };
+    uploadAudio();
+  }, [audioFile]);
 
   return {
     startRecording,
     stopRecording,
     isAudio,
+    transcript,
+    isLoading,
   };
 };
