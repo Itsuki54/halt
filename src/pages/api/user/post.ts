5370f92747e3e0a128bf643e052a33f82821b542
Itsuki54 <5shi.icyu@gmail.com>
2024-10-25 01:58:30 +0900

    rm crypt

--- src/pages/api/user/post.ts
+++ src/pages/api/user/post.ts
@@ -1,29 +1,18 @@
 import { db } from '@/lib/prisma';
-import bcrypt from 'bcrypt';
-import crypto from 'crypto';
+
 import {
   NextApiRequest,
   NextApiResponse,
 } from 'next';
 
-const genSalt = () => {
-  const S = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  return Array.from(crypto.randomFillSync(new Uint32Array(24)))
-    .map(v => S[v % S.length])
-    .join('');
-};
-
 export default async function post(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const { email, password, type } = req.body;
-    if (!email || !password) {
+    const { email } = req.body;
+    if (!email) {
       return res
         .status(400)
         .json({ status: 'error', error: 'User Data not provided' });
     }
-    const pepper = process.env.PEPPER;
-    const salt = genSalt();
-    const hash = await bcrypt.hash(password + pepper, salt);
     const user = await db.user.create({
       data: {
         email,
