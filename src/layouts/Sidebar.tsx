b4572e03f6fa50b1808c49c7f29ff9b3cf9413ff
kazuki <kitano-kazuki954@e.c..u-tokyo.ac.jp>
2024-10-25 19:12:37 +0900

    conversation.pngがないからそれをcomment outした

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -9,7 +9,7 @@ export function Sidebar() {
       <div className='flex flex-col' style={{ gap: '20px' }}>
         <div className='flex items-center border bg-black' onClick={() => router.push('/#')} style={{ width: '50%', backgroundColor: 'rgba(0, 195, 202, 0.3)', borderColor: 'rgb(0, 195, 202)' }}>
           <div className='bg-blue-500 flex items-center justify-center p-1 h-full' style={{ width: '20%', backgroundColor: 'rgba(0, 195, 202, 1)' }}>
-            <Image alt='text chat' className='w-full' src='/conversation.png' />
+            {/* <Image alt='text chat' className='w-full' src='/conversation.png' /> */}
           </div>
           <div className='text-2xl flex-1 text-center basis-3/4 h-full flex items-center justify-center font-bold'>
             Text Chat
