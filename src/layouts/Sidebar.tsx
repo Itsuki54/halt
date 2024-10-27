b7a97fd283e887b04b45f8eae5888075999030a3
kitano kazuki <kitano-kazuki954@g.ecc.u-tokyo.ac.jp>
2024-10-27 15:06:10 +0900

    Imageを react iconに変更

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { FiMessageSquare, FiSettings } from 'react-icons/fi';
 import { useRouter } from 'next/router';
 
 export function Sidebar() {
@@ -9,7 +10,7 @@ export function Sidebar() {
       <div className='flex flex-col' style={{ gap: '20px' }}>
         <div className='flex items-center border bg-black' onClick={() => router.push('/#')} style={{ width: '100%', backgroundColor: 'rgba(0, 195, 202, 0.3)', borderColor: 'rgb(0, 195, 202)' }}>
           <div className='bg-blue-500 flex items-center justify-center p-1 h-full' style={{ width: '20%', backgroundColor: 'rgba(0, 195, 202, 1)' }}>
-            <Image alt='text chat' width={30} height={30} src='/conversation.png' />
+            <FiMessageSquare size={30} color="white" />
           </div>
           <div className='text-2xl flex-1 text-center basis-3/4 h-full flex items-center justify-center font-bold'>
             Text Chat
@@ -17,7 +18,7 @@ export function Sidebar() {
         </div>
         <div className='flex items-center border bg-black' onClick={() => router.push('/#')} style={{ width: '100%', backgroundColor: 'rgba(74, 218, 156, 0.3)', borderColor: 'rgb(74, 218, 156)' }}>
           <div className='bg-blue-500 flex items-center justify-center p-1 h-full' style={{ width: '20%', backgroundColor: 'rgba(74, 218, 156, 1)' }}>
-            <Image alt='setting' width={30} height={30} src='/setting.png' />
+            <FiSettings size={30} color="white" />
           </div>
           <div className='text-2xl flex-1 text-center basis-3/4 h-full flex items-center justify-center font-bold'>
             User Setting
