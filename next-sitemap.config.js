105987560547b78321ba53e7ef9170b8dda25988
Itsuki54 <104209850+Itsuki54@users.noreply.github.com>
2024-10-28 11:14:56 +0900

    next-sitemap.config.js を更新

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,2 +1,4 @@
-export const siteUrl = 'https://prototype-connect2u.vercel.app';
-export const generateRobotsTxt = true;
+export default {
+  siteUrl: 'https://prototype-connect2u.vercel.app',
+  generateRobotsTxt: true,
+};
